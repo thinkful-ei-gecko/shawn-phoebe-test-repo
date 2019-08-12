7f61cc33e7bd448039b8c8ab4943b25d0f4adceb
Phoebe Law <phoebebasilio@gmail.com>
2019-08-12 11:56:06 -0700

    name and age throw errors

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 /* eslint-disable strict */
 
+
+//8) What happens if you pass "29" (string) as the age? What about "twenty nine"? Implement a check to ensure the name is a string and the age is a number. If not, throw new TypeError().
+
+Hint: typeof age Investigate: What is NaN?
 function getYearOfBirth(age) {
   if (age < 0) {
     throw new Error("Age can not be negative");
@@ -11,6 +15,15 @@ function createGreeting(name, age) {
   if (name == undefined || age == undefined) {
     throw new Error('Arguments not valid');
   }
+
+  if (typeof name !== 'string') {
+    throw new Error ('Name is not a string');
+  }
+
+  if (typeof age !== 'number') {
+    throw new Error ('Age is not a number');
+  }
+
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I\'m 29 years old. I was born in ${yearOfBirth}`;
 }
@@ -30,7 +43,6 @@ catch{
 }
 
 
-
-const greeting1 = createGreeting(-15);
+const greeting1 = createGreeting(Chris, "15");
 console.log(greeting1);
 
