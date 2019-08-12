1e8504241a33999146463e1177a11f56f5321bc1
Shawn-Collette <shawn.collette@gmail.com>
2019-08-12 12:29:55 -0600

    try catch

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-// eslint-disable-next-line strict
-function createGreeting() {
-  return 'Hi, my name is Chris and I\'m 29 years old';
+/* eslint-disable strict */
+
+function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error("Age can not be negative");
+  }
+  return 2019 - age;
 }
 
-const greeting1 = createGreeting();
+function createGreeting(name, age) {
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m 29 years old. I was born in ${yearOfBirth}`;
+}
+
+try {
+  getYearOfBirth();
+}
+catch{
+  console.error(Error);
+}
+
+const greeting1 = createGreeting('Chris', -15);
 console.log(greeting1);
+
